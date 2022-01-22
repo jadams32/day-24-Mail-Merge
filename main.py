a859a9c99dad7e75e75498ea426b537e881771ef
John Adams Jr <john.c.adams32@gmail.com>
2022-01-22 11:49:23 -0700

    file structure fixed, comments complete

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# On Day 24 of 100 days of code, the main topic learned was reading/writing to files, and file path structures.
+# Here we use that knowledge by creating a mail merge program to send birthday letters out to friends.
 
 # Create our letter
-with open("starting_letter.txt", mode="r") as letter:
+with open("./Input/Letters/starting_letter.txt", mode="r") as letter:
     letter = letter.read()
 
 # Create a list of names to merge
-with open("Mail Merge Project Start/Input/Names/invited_names.txt", mode="r") as name_data:
+with open("./Input/Names/invited_names.txt", mode="r") as name_data:
     names = name_data.readlines()
-
-print(names)
-
-# Save letter to a new file in the folder output
-for name in names:
-    with open(f"day/Output/ReadyToSend/letter_for_{name}.txt", mode="w") as final_letter:
-        # Replace text in letter with actual name
-        final_letter.write(letter.replace("[name]", name))
-
-
-
+    # Go through the names list and strip any spaces
+    for name in names:
+        formatted_name = name.strip()
+        # Save letter to a new file in the folder output
+        with open(f"./Output/ReadyToSend/letter_for_{formatted_name}.txt", mode="w") as final_letter:
+            # Replace text in letter with actual name
+            final_letter.write(letter.replace("[name]", formatted_name))
         
