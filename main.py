acb299fd2cf73616472123e3a448c4e8050fdee5
John Adams Jr <john.c.adams32@gmail.com>
2022-01-22 11:28:58 -0700

    need to fix file structure

--- main.py
+++ main.py
@@ -7,12 +7,22 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
+# Create our letter
+with open("starting_letter.txt", mode="r") as letter:
+    letter = letter.read()
+
+# Create a list of names to merge
 with open("Mail Merge Project Start/Input/Names/invited_names.txt", mode="r") as name_data:
     names = name_data.readlines()
 
 print(names)
 
-with open("starting_letter.txt", mode="w") as letter:
-    for name in names:
-        letter.replace("[name]", name)
+# Save letter to a new file in the folder output
+for name in names:
+    with open(f"day/Output/ReadyToSend/letter_for_{name}.txt", mode="w") as final_letter:
+        # Replace text in letter with actual name
+        final_letter.write(letter.replace("[name]", name))
+
+
+
         
